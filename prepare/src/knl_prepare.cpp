be1d66cb50c4fadfc82fd590bac48f97051205dd
Xinping Deng <xinping.deng@gmail.com>
2019-11-08 09:27:18 +1100

    backup

--- prepare/src/knl_prepare.cpp
+++ prepare/src/knl_prepare.cpp
@@ -12,14 +12,14 @@ extern "C" {
 		   burst_data_type *average_pol1,
 		   burst_data_type *average_pol2   
 		   ){
-#pragma HLS INTERFACE m_axi port = in_pol1 offset = slave bundle = gmem0 max_read_burst_length=64
-#pragma HLS INTERFACE m_axi port = in_pol2 offset = slave bundle = gmem1 max_read_burst_length=64
-#pragma HLS INTERFACE m_axi port = cal_pol1 offset = slave bundle = gmem2 max_read_burst_length=64
-#pragma HLS INTERFACE m_axi port = cal_pol2 offset = slave bundle = gmem3 max_read_burst_length=64
-#pragma HLS INTERFACE m_axi port = sky offset = slave bundle = gmem4 max_read_burst_length=64
-#pragma HLS INTERFACE m_axi port = out offset = slave bundle = gmem5 max_write_burst_length=64
-#pragma HLS INTERFACE m_axi port = average_pol1 offset = slave bundle = gmem6 max_write_burst_length=64
-#pragma HLS INTERFACE m_axi port = average_pol2 offset = slave bundle = gmem7 max_write_burst_length=64
+#pragma HLS INTERFACE m_axi port = in_pol1 offset = slave bundle = gmem0 max_read_burst_length=64 depth=64
+#pragma HLS INTERFACE m_axi port = in_pol2 offset = slave bundle = gmem1 max_read_burst_length=64 depth=64
+#pragma HLS INTERFACE m_axi port = cal_pol1 offset = slave bundle = gmem2 max_read_burst_length=64 depth=64
+#pragma HLS INTERFACE m_axi port = cal_pol2 offset = slave bundle = gmem3 max_read_burst_length=64 depth=64
+#pragma HLS INTERFACE m_axi port = sky offset = slave bundle = gmem4 max_read_burst_length=64 depth=64
+#pragma HLS INTERFACE m_axi port = out offset = slave bundle = gmem5 max_write_burst_length=64 depth=64
+#pragma HLS INTERFACE m_axi port = average_pol1 offset = slave bundle = gmem6 max_write_burst_length=64 depth=64
+#pragma HLS INTERFACE m_axi port = average_pol2 offset = slave bundle = gmem7 max_write_burst_length=64 depth=64
 
 #pragma HLS INTERFACE s_axilite port = in_pol1 bundle = control
 #pragma HLS INTERFACE s_axilite port = in_pol2 bundle = control
@@ -49,14 +49,15 @@ extern "C" {
     burst_data_type out_burst;
     burst_data_type average_pol1_burst[MAX_BURST_LENGTH];	       
     burst_data_type average_pol2_burst[MAX_BURST_LENGTH];
-    
+        
     int i;
     int j;
     int m;
     int n;
     int loc;
+    int itran;
     
-  LOOP_BURST_IN_CAL_SKY_RESET_AVERAGE:
+  LOOP_BURST_CAL_SKY_RESET_AVERAGE1:
     for(i = 0; i < MAX_BURST_LENGTH; i++){
 #pragma HLS PIPELINE 
       cal_pol1_burst[i] = cal_pol1[i];
@@ -72,57 +73,146 @@ extern "C" {
     }
     
   LOOP_NTRAN_PER_TIME:
-    for(i = 0; i < NTRAN_PER_TIME; i++){
-    LOOP_NTIME_PER_CU:
+    for(i = 0; i < NTRAN_PER_TIME - 1; i++){
+    LOOP_NTIME_PER_CU1:
       for(j = 0; j < NTIME_PER_CU; j++){	
-      LOOP_CAL_AVERAGE_OUT_M:
-	for(m = 0; m < MAX_BURST_LENGTH; m++)
-	  {
-#pragma HLS PIPELINE 
-	    loc = j*NTRAN_PER_TIME*MAX_BURST_LENGTH + i*MAX_BURST_LENGTH + m;	  
-	    in_pol1_burst = in_pol1[loc];
-	    in_pol2_burst = in_pol2[loc];
-	  LOOP_CAL_AVERAGE_OUT_N:
-	    for(n = 0; n < NSAMP_PER_BURST; n++){
+      LOOP_CAL_AVERAGE_OUT_M1:
+	for(m = 0; m < MAX_BURST_LENGTH; m++){
+#pragma HLS PIPELINE
+	  loc = j*NBURST_PER_TIME + i*MAX_BURST_LENGTH + m; 
+	  in_pol1_burst = in_pol1[loc];
+	  in_pol2_burst = in_pol2[loc];
+	LOOP_CAL_AVERAGE_OUT_N1:
+	  for(n = 0; n < NSAMP_PER_BURST; n++){
 #pragma HLS UNROLL
-	      average_pol1_burst[m].data[2*n]   += in_pol1_burst.data[2*n];
-	      average_pol1_burst[m].data[2*n+1] += in_pol1_burst.data[2*n+1];
-	      average_pol2_burst[m].data[2*n]   += in_pol2_burst.data[2*n];
-	      average_pol2_burst[m].data[2*n+1] += in_pol2_burst.data[2*n+1];
+	    average_pol1_burst[m].data[2*n]   += in_pol1_burst.data[2*n];
+	    average_pol1_burst[m].data[2*n+1] += in_pol1_burst.data[2*n+1];
+	    average_pol2_burst[m].data[2*n]   += in_pol2_burst.data[2*n];
+	    average_pol2_burst[m].data[2*n+1] += in_pol2_burst.data[2*n+1];
 	      
-	      out_burst.data[2*n] = in_pol1_burst.data[2*n]*cal_pol1_burst[m].data[2*n] - in_pol1_burst.data[2*n+1]*cal_pol1_burst[m].data[2*n+1] + 
-		in_pol2_burst.data[2*n]*cal_pol2_burst[m].data[2*n] - in_pol2_burst.data[2*n+1]*cal_pol2_burst[m].data[2*n+1] - 
-		sky_burst[m].data[2*n];
+	    out_burst.data[2*n] = in_pol1_burst.data[2*n]*cal_pol1_burst[m].data[2*n] - in_pol1_burst.data[2*n+1]*cal_pol1_burst[m].data[2*n+1] + 
+	      in_pol2_burst.data[2*n]*cal_pol2_burst[m].data[2*n] - in_pol2_burst.data[2*n+1]*cal_pol2_burst[m].data[2*n+1] - 
+	      sky_burst[m].data[2*n];
 	      
-	      out_burst.data[2*n+1] = in_pol1_burst.data[2*n]*cal_pol1_burst[m].data[2*n+1] + in_pol1_burst.data[2*n+1]*cal_pol1_burst[m].data[2*n] + 
-		in_pol2_burst.data[2*n]*cal_pol2_burst[m].data[2*n+1] + in_pol2_burst.data[2*n+1]*cal_pol2_burst[m].data[2*n] - 
-		sky_burst[m].data[2*n+1];
-	    }
-	    out[loc] = out_burst;
+	    out_burst.data[2*n+1] = in_pol1_burst.data[2*n]*cal_pol1_burst[m].data[2*n+1] + in_pol1_burst.data[2*n+1]*cal_pol1_burst[m].data[2*n] + 
+	      in_pol2_burst.data[2*n]*cal_pol2_burst[m].data[2*n+1] + in_pol2_burst.data[2*n+1]*cal_pol2_burst[m].data[2*n] - 
+	      sky_burst[m].data[2*n+1];
 	  }
+	  out[loc] = out_burst;
+	}
       }
       
-    LOOP_BURST_OUT_AVERAGE:
+    LOOP_BURST_AVERAGE1:
       for(j = 0; j < MAX_BURST_LENGTH; j++){
 #pragma HLS PIPELINE 
 	loc = i*MAX_BURST_LENGTH + j;
 	average_pol1[loc] = average_pol1_burst[j];
 	average_pol2[loc] = average_pol2_burst[j];
+      	
+	loc += MAX_BURST_LENGTH;
+	cal_pol1_burst[j] = cal_pol1[loc];
+	cal_pol2_burst[j] = cal_pol2[loc];
+	sky_burst[j]      = sky[loc];
 	
-	if(i<(NTRAN_PER_TIME-1)){
-	  loc += MAX_BURST_LENGTH;
-	  cal_pol1_burst[j] = cal_pol1[loc];
-	  cal_pol2_burst[j] = cal_pol2[loc];
-	  sky_burst[j]      = sky[loc];
-	
-	LOOP_RESET_AVERAGE2:
-	  for(m = 0; m < NDATA_PER_BURST; m++){
+      LOOP_RESET_AVERAGE2:
+	for(m = 0; m < NDATA_PER_BURST; m++){
 #pragma HLS UNROLL
-	    average_pol1_burst[j].data[m] = 0;
-	    average_pol2_burst[j].data[m] = 0;
-	  }
+	  average_pol1_burst[j].data[m] = 0;
+	  average_pol2_burst[j].data[m] = 0;
 	}
       }      
     }
+
+    //////////////////////////////////////////
+    itran = NTRAN_PER_TIME - 1;
+  LOOP_NTIME_PER_CU2:
+    for(j = 0; j < NTIME_PER_CU; j++){	
+    LOOP_CAL_AVERAGE_OUT_M2:
+      for(m = 0; m < MAX_BURST_LENGTH; m++){
+#pragma HLS PIPELINE
+	loc = j*NBURST_PER_TIME + itran*MAX_BURST_LENGTH + m; 
+	in_pol1_burst = in_pol1[loc];
+	in_pol2_burst = in_pol2[loc];
+      LOOP_CAL_AVERAGE_OUT_N2:
+	for(n = 0; n < NSAMP_PER_BURST; n++){
+#pragma HLS UNROLL
+	  average_pol1_burst[m].data[2*n]   += in_pol1_burst.data[2*n];
+	  average_pol1_burst[m].data[2*n+1] += in_pol1_burst.data[2*n+1];
+	  average_pol2_burst[m].data[2*n]   += in_pol2_burst.data[2*n];
+	  average_pol2_burst[m].data[2*n+1] += in_pol2_burst.data[2*n+1];
+	  
+	  out_burst.data[2*n] = in_pol1_burst.data[2*n]*cal_pol1_burst[m].data[2*n] - in_pol1_burst.data[2*n+1]*cal_pol1_burst[m].data[2*n+1] + 
+	    in_pol2_burst.data[2*n]*cal_pol2_burst[m].data[2*n] - in_pol2_burst.data[2*n+1]*cal_pol2_burst[m].data[2*n+1] - 
+	    sky_burst[m].data[2*n];
+	      
+	  out_burst.data[2*n+1] = in_pol1_burst.data[2*n]*cal_pol1_burst[m].data[2*n+1] + in_pol1_burst.data[2*n+1]*cal_pol1_burst[m].data[2*n] + 
+	    in_pol2_burst.data[2*n]*cal_pol2_burst[m].data[2*n+1] + in_pol2_burst.data[2*n+1]*cal_pol2_burst[m].data[2*n] - 
+	    sky_burst[m].data[2*n+1];
+	}
+	out[loc] = out_burst;
+      }
+    }
+    
+  LOOP_BURST_AVERAGE2:
+    for(j = 0; j < MAX_BURST_LENGTH; j++){
+#pragma HLS PIPELINE 
+      loc = itran*MAX_BURST_LENGTH + j;
+      average_pol1[loc] = average_pol1_burst[j];
+      average_pol2[loc] = average_pol2_burst[j];
+    }
+    
+  LOOP_BURST_CAL_SKY_RESET_AVERAGE2:
+    for(j = 0; j < NBURST_PER_TIME_REMIND; j++){    
+      loc = (itran+1)*MAX_BURST_LENGTH+j;
+      cal_pol1_burst[j] = cal_pol1[loc];
+      cal_pol2_burst[j] = cal_pol2[loc];
+      sky_burst[j]      = sky[loc];
+      
+    LOOP_RESET_AVERAGE3:
+      for(m = 0; m < NDATA_PER_BURST; m++){
+#pragma HLS UNROLL
+	average_pol1_burst[j].data[m] = 0;
+	average_pol2_burst[j].data[m] = 0;
+      }
+    }
+    
+    //////////////////////////
+    itran = NTRAN_PER_TIME;
+  LOOP_NTIME_PER_CU3:
+    for(j = 0; j < NTIME_PER_CU; j++){	
+    LOOP_CAL_AVERAGE_OUT_M3:
+      for(m = 0; m < NBURST_PER_TIME_REMIND; m++){
+#pragma HLS PIPELINE
+	loc = j*NBURST_PER_TIME + itran*MAX_BURST_LENGTH + m; 
+	in_pol1_burst = in_pol1[loc];
+	in_pol2_burst = in_pol2[loc];
+      LOOP_CAL_AVERAGE_OUT_N3:
+	for(n = 0; n < NSAMP_PER_BURST; n++){
+#pragma HLS UNROLL
+	  average_pol1_burst[m].data[2*n]   += in_pol1_burst.data[2*n];
+	  average_pol1_burst[m].data[2*n+1] += in_pol1_burst.data[2*n+1];
+	  average_pol2_burst[m].data[2*n]   += in_pol2_burst.data[2*n];
+	  average_pol2_burst[m].data[2*n+1] += in_pol2_burst.data[2*n+1];
+	  
+	  out_burst.data[2*n] = in_pol1_burst.data[2*n]*cal_pol1_burst[m].data[2*n] - in_pol1_burst.data[2*n+1]*cal_pol1_burst[m].data[2*n+1] + 
+	    in_pol2_burst.data[2*n]*cal_pol2_burst[m].data[2*n] - in_pol2_burst.data[2*n+1]*cal_pol2_burst[m].data[2*n+1] - 
+	    sky_burst[m].data[2*n];
+	  
+	  out_burst.data[2*n+1] = in_pol1_burst.data[2*n]*cal_pol1_burst[m].data[2*n+1] + in_pol1_burst.data[2*n+1]*cal_pol1_burst[m].data[2*n] + 
+	    in_pol2_burst.data[2*n]*cal_pol2_burst[m].data[2*n+1] + in_pol2_burst.data[2*n+1]*cal_pol2_burst[m].data[2*n] - 
+	    sky_burst[m].data[2*n+1];
+	}
+	out[loc] = out_burst;
+      }
+    }
+    
+  LOOP_BURST_AVERAGE3:
+    for(j = 0; j < NBURST_PER_TIME_REMIND; j++){
+#pragma HLS PIPELINE 
+      loc = itran*MAX_BURST_LENGTH + j;
+      average_pol1[loc] = average_pol1_burst[j];
+      average_pol2[loc] = average_pol2_burst[j];
+    }    
+    ///////////////////////////////////////////////
   }
 }
--- prepare/src/prepare.h
+++ prepare/src/prepare.h
@@ -15,29 +15,29 @@
 #include <complex>
 #include <ap_fixed.h>
 
-#define NDATA_PER_SAMP      2      // We are using complex numbers
-//#define CORE_DATA_WIDTH     32    // We are using float complex numbers
-#define CORE_DATA_WIDTH     16    // We are using float complex numbers
-#define COMPUTE_DATA_WIDTH  (NDATA_PER_SAMP*CORE_DATA_WIDTH)   // complex float
-
-#define BURST_DATA_WIDTH    512  // Memory width of xilinx, hard limit
-#define MAX_BURST_DATA_SIZE 32768 // Max data size in bits per transaction 4KB
-#define MAX_BURST_LENGTH    (MAX_BURST_DATA_SIZE/BURST_DATA_WIDTH)
-
-#define NSAMP_PER_BURST     (BURST_DATA_WIDTH/COMPUTE_DATA_WIDTH) 
-#define NDATA_PER_BURST     (NDATA_PER_SAMP*NSAMP_PER_BURST)
-
-#define NCHAN               288
-#define NTIME_PER_CU        256    // For each compute unit
-#define NBASELINE           416
-
-//#define NCHAN               288
-//#define NTIME_PER_CU        5    // For each compute unit
-//#define NBASELINE           32
-
-#define NSAMP_PER_TIME      (NCHAN*NBASELINE)
-#define NBURST_PER_TIME     (NSAMP_PER_TIME/NSAMP_PER_BURST)
-#define NTRAN_PER_TIME      (NBURST_PER_TIME/MAX_BURST_LENGTH)
+#define NDATA_PER_SAMP      	2      // We are using complex numbers
+#define CORE_DATA_WIDTH     	16    // We are using float complex numbers
+#define COMPUTE_DATA_WIDTH  	(NDATA_PER_SAMP*CORE_DATA_WIDTH)   // complex float
+			    	
+#define BURST_DATA_WIDTH    	512  // Memory width of xilinx, hard limit
+#define MAX_BURST_DATA_SIZE 	32768 // Max data size in bits per transaction 4KB
+#define MAX_BURST_LENGTH    	(MAX_BURST_DATA_SIZE/BURST_DATA_WIDTH)
+			    	
+#define NSAMP_PER_BURST     	(BURST_DATA_WIDTH/COMPUTE_DATA_WIDTH) 
+#define NDATA_PER_BURST     	(NDATA_PER_SAMP*NSAMP_PER_BURST)
+			    	
+#define NCHAN             	  288
+#define NTIME_PER_CU      	  256    // For each compute unit
+#define NBASELINE         	  435
+			    	
+//#define NCHAN               	288
+//#define NTIME_PER_CU        	10    // For each compute unit
+//#define NBASELINE           	15
+			    	
+#define NSAMP_PER_TIME      	(NCHAN*NBASELINE)
+#define NBURST_PER_TIME     	(NSAMP_PER_TIME/NSAMP_PER_BURST)   // It is hard to pick up remainder here
+#define NTRAN_PER_TIME      	(NBURST_PER_TIME/MAX_BURST_LENGTH) // It is okay to pick up remainder here
+#define NBURST_PER_TIME_REMIND  (NBURST_PER_TIME%MAX_BURST_LENGTH)
 
 typedef ap_fixed<CORE_DATA_WIDTH, CORE_DATA_WIDTH/2> core_data_type; // The size of this should be CORE_DATA_WIDTH
 typedef std::complex<core_data_type> compute_data_type; // The size of it should be COMPUTE_DATA_WIDTH
