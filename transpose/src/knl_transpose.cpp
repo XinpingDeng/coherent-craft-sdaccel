fe9ad35e016d8f4152ba9cf257d6fbb1c0f6f247
Keith Bannister <keith.bannister@csiro.au>
2020-01-20 10:53:59 +1100

    backup

--- transpose/src/knl_transpose.cpp
+++ transpose/src/knl_transpose.cpp
@@ -21,9 +21,16 @@ void knl_transpose(
                    int nburst_dm
                    )
 {
-#pragma HLS INTERFACE m_axi port = in    offset = slave bundle = gmem0 
+  const int nsamp_per_burst   = NSAMP_PER_BURST;
+  const int ndata_per_burst   = NDATA_PER_BURST;
+  const int mtime_per_cu      = MTIME_PER_CU;
+  const int mtran_per_uv_out  = MBURST_PER_UV_OUT/BURST_LENGTH;
+  const int mtran_dm          = MBURST_DM/BURST_LENGTH;
+  const int mburst_per_uv_out = MBURST_PER_UV_OUT;
+
+#pragma HLS INTERFACE m_axi port = in    offset = slave bundle = gmem0 max_read_burst_length=nsamp_per_burst
 #pragma HLS INTERFACE m_axi port = coord offset = slave bundle = gmem1 
-#pragma HLS INTERFACE m_axi port = out   offset = slave bundle = gmem2 max_write_burst_length=64
+#pragma HLS INTERFACE m_axi port = out   offset = slave bundle = gmem2 max_write_burst_length=nsamp_per_burst
 
 #pragma HLS INTERFACE s_axilite port = in     bundle = control
 #pragma HLS INTERFACE s_axilite port = coord  bundle = control
@@ -42,21 +49,22 @@ void knl_transpose(
   int j;
   int m;
   int n;
+  int m1;
+  int n1;
   int k;
-  burst_uv in_burst[NSAMP_PER_BURST];
-  burst_uv out_burst[NSAMP_PER_BURST];
-  int loc_inburst;
-  int loc_outburst;
-  int loc_in;
-  int loc_out;
+  burst_uv out_burst;
+  uv_t buffer[NDATA_PER_BURST*BURST_LENGTH][NDATA_PER_BURST*BURST_LENGTH];
+  int loc_burst;
+  int loc_uv;
+  int loc_dm;
   int loc_coord;
   coord_t2 start[MSAMP_PER_UV_OUT];
-  
-  const int nsamp_per_burst   = NSAMP_PER_BURST;
-  const int mtime_per_cu      = MTIME_PER_CU;
-  const int mburst_per_uv_out = MBURST_PER_UV_OUT;
-  const int mburst_dm         = MBURST_DM;
-#pragma HLS ARRAY_RESHAPE variable=start cyclic factor = nsamp_per_burst
+    
+#pragma HLS ARRAY_RESHAPE variable=buffer cyclic factor = nsamp_per_burst dim = 1
+#pragma HLS ARRAY_RESHAPE variable=buffer cyclic factor = ndata_per_burst dim = 2
+#pragma HLS ARRAY_RESHAPE variable=start  cyclic factor = nsamp_per_burst
+
+  //FILE *fp = fopen("/data/FRIGG_2/Workspace/coherent-craft-sdaccel/transpose/src/knl_error.txt", "w");
   
  LOOP_COORD:
   for(i = 0; i < nburst_per_uv_out; i++){
@@ -68,39 +76,55 @@ void knl_transpose(
     }    
   }
   
-  for(i = 0; i < nburst_per_uv_out; i++){
-#pragma HLS LOOP_TRIPCOUNT max=mburst_per_uv_out
-    for(j = 0; j < ntime_per_cu; j++){
+  for(k = 0; k < ntime_per_cu; k++){
 #pragma HLS LOOP_TRIPCOUNT max=mtime_per_cu
-    LOOP_TRANSPOSE:
-      for(m = 0; m < nburst_dm; m++){
-#pragma HLS PIPELINE II=nsamp_per_burst
-#pragma HLS LOOP_TRIPCOUNT max=mburst_dm
-        // Read in
-        for(n = 0; n < NSAMP_PER_BURST; n++){
-          loc_inburst = start[i*NSAMP_PER_BURST+n]*ntime_per_cu*nburst_dm +
-            j*nburst_dm+
-            m;
-          in_burst[n] = in[loc_inburst];
-        }
-
-        // Transpose
-        for(n = 0; n < NSAMP_PER_BURST; n++){
-          for(k = 0; k < NSAMP_PER_BURST; k++){
-            out_burst[n].data[2*k]   = in_burst[k].data[2*n];
-            out_burst[n].data[2*k+1] = in_burst[k].data[2*n+1];
+    for(i = 0; i < nburst_per_uv_out/(2*BURST_LENGTH); i++){
+#pragma HLS LOOP_TRIPCOUNT max=mtran_per_uv_out
+      for(j = 0; j < nburst_dm/BURST_LENGTH; j++){
+#pragma HLS LOOP_TRIPCOUNT max=mtran_dm
+        
+        // Buffer data for transpose later
+        for(m1 = 0; m1 < BURST_LENGTH; m1++){     // For UV
+          for(m = 0; m < NDATA_PER_BURST; m++){   // For UV
+            loc_uv = m1*NDATA_PER_BURST+m;
+          LOOP_IN:
+            for(n1 = 0; n1 < BURST_LENGTH; n1++){ // For DM
+#pragma HLS PIPELINE
+              loc_burst = start[(i*BURST_LENGTH+m1)*NDATA_PER_BURST+m]*ntime_per_cu*nburst_dm +
+                k*nburst_dm+
+                j*BURST_LENGTH + n1;
+              
+              for(n = 0; n < NDATA_PER_BURST; n++){
+                loc_dm = n1*NDATA_PER_BURST+n;
+                buffer[loc_uv][loc_dm] = in[loc_burst].data[n];
+                //fprintf(fp, "%f\n", buffer[loc_uv][loc_dm].to_float());
+              }
+            }
           }
         }
-
+        
         // Write out
-        for(n = 0; n < NSAMP_PER_BURST; n++){
-          loc_outburst = (m*NSAMP_PER_BURST+n)*ntime_per_cu*nburst_per_uv_out +
-            j*nburst_per_uv_out+
-            i;            
-          out[loc_outburst] = out_burst[n];
+        for(m1 = 0; m1 < BURST_LENGTH; m1++){     // For DM
+          for(m = 0; m < NDATA_PER_BURST; m++){   // For DM
+            loc_dm = m1*NDATA_PER_BURST+m;
+          LOOP_OUT:
+            for(n1 = 0; n1 < BURST_LENGTH; n1++){ // For UV
+#pragma HLS PIPELINE
+              loc_burst = (j*NDATA_PER_BURST+m1)*ntime_per_cu*nburst_per_uv_out +
+                k*nburst_per_uv_out+
+                i*BURST_LENGTH+n1;
+              
+              for(n = 0; n < NDATA_PER_BURST; n++){
+                loc_uv = n1*NDATA_PER_BURST+n;
+                out_burst.data[n]   = buffer[loc_uv][loc_dm];
+              }
+              out[loc_burst] = out_burst;
+            }
+          }
         }
       }
     }
   }
-}
 
+  //fclose(fp);
+}
