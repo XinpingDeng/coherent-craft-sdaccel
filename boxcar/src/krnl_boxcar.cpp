889e843f032c88753a5d19d95a796fb29e858def
xinpingdeng <keith.bannister@csiro.au>
2020-02-28 10:36:51 +1100

    backup

--- boxcar/src/krnl_boxcar.cpp
+++ boxcar/src/krnl_boxcar.cpp
@@ -7,9 +7,8 @@ extern "C"{
                    burst_real *history1,
                    int ndm,
                    int ntime,
-                   int nburst_per_img,
                    real_t threshold,
-                   real_t *out
+                   burst_real *out
                    );
   
   void read_history(
@@ -24,9 +23,8 @@ extern "C"{
                fifo_burst &history1_fifo,
                int ndm,
                int ntime,
-               int nburst_per_img,
                real_t threshold,
-               real_t *out);
+               burst_real *out);
   
   void write_history(
                      int nburst_history,
@@ -35,16 +33,21 @@ extern "C"{
                      );
 
   void fifo2history(
-                    int nburst_per_img,
                     fifo_burst &history_fifo,
-                    real_t history[MAX_SMP_PER_IMG*(NBOXCAR-1)]
+                    real_t history[NSMP_PER_IMG*(NBOXCAR-1)]
                     );
   
   void history2fifo(
-                    int nburst_per_img,
-                    real_t history[MAX_SMP_PER_IMG*(NBOXCAR-1)],
+                    real_t history[NSMP_PER_IMG*(NBOXCAR-1)],
                     fifo_burst &history_fifo
                     );
+
+  void process_dm(
+                  int ntime,
+                  stream_burst &in, 
+                  real_t history[NSMP_PER_IMG*(NBOXCAR-1)],
+                  real_t threshold,
+                  burst_real *out);    
 }
 
 void krnl_boxcar(
@@ -53,9 +56,8 @@ void krnl_boxcar(
                  burst_real *history1,
                  int ndm,
                  int ntime,
-                 int nburst_per_img,
                  real_t threshold,
-                 real_t *out
+                 burst_real *out
                  ){
   const int max_burst_length = BURST_LEN_BOXCAR;
   
@@ -63,26 +65,27 @@ void krnl_boxcar(
 #pragma HLS INTERFACE m_axi port = history1 offset = slave bundle = gmem1 max_read_burst_length=max_burst_length
 #pragma HLS INTERFACE m_axi port = out      offset = slave bundle = gmem2 max_read_burst_length=max_burst_length
 #pragma HLS INTERFACE axis port  = in
-  
+    
 #pragma HLS INTERFACE s_axilite port = history0       bundle = control
 #pragma HLS INTERFACE s_axilite port = history1       bundle = control
 #pragma HLS INTERFACE s_axilite port = ndm            bundle = control
 #pragma HLS INTERFACE s_axilite port = ntime          bundle = control
-#pragma HLS INTERFACE s_axilite port = nburst_per_img bundle = control
 #pragma HLS INTERFACE s_axilite port = threshold      bundle = control  
 #pragma HLS INTERFACE s_axilite port = return         bundle = control
-
+  
 #pragma HLS DATA_PACK variable = history0
 #pragma HLS DATA_PACK variable = history1
+#pragma HLS DATA_PACK variable = out
 
-  int nburst_history = nburst_per_img*ndm*(NBOXCAR-1);
+ nburst_history:
+  int nburst_history = NBURST_PER_IMG*ndm*(NBOXCAR-1);
   fifo_burst history0_fifo;
   fifo_burst history1_fifo; 
 #pragma HLS STREAM variable=history0_fifo
 #pragma HLS STREAM variable=history1_fifo 
 #pragma HLS DATAFLOW  
   read_history(nburst_history, history0, history0_fifo);
-  process(in, history0_fifo, history1_fifo, ndm, ntime, nburst_per_img, threshold, out);
+  process(in, history0_fifo, history1_fifo, ndm, ntime, threshold, out);
   write_history(nburst_history, history1, history1_fifo);
 }
 
@@ -125,68 +128,38 @@ void process(
              fifo_burst &history1_fifo,
              int ndm,
              int ntime,
-             int nburst_per_img,
              real_t threshold,
-             real_t *out){
+             burst_real *out){
   int i;
-  real_t history[MAX_SMP_PER_IMG*(NBOXCAR-1)];
-
+  real_t history[NSMP_PER_IMG*(NBOXCAR-1)];
+  
   const int max_dm = MAX_DM;
   const int nsmp_per_process = (NBOXCAR-1)*NREAL_PER_BURST;
-  //const int nsmp_per_process = NREAL_PER_BURST;
-  //#pragma HLS array_reshape   variable=history cyclic factor=nsmp_per_process
+
 #pragma HLS array_reshape   variable=history block factor=nsmp_per_process
   //#pragma HLS array_partition variable=history cyclic factor=nsmp_per_process
-  
-    for(i = 0; i < ndm; i++){
+    
+  for(i = 0; i < ndm; i++){
 #pragma HLS LOOP_TRIPCOUNT max=max_dm
-      //#pragma HLS DATAFLOW
-      fifo2history(nburst_per_img, history0_fifo, history);
-      //process_worker(ntime, in, history, threshold, cand);    
-      history2fifo(nburst_per_img, history, history1_fifo);
-    }
+#pragma HLS DATAFLOW
+    fifo2history(history0_fifo, history);
+    process_dm(ntime, in, history, threshold, out);    
+    history2fifo(history, history1_fifo);
+  }
 }
 
-//void fifo2history(
-//                  int nburst_per_img,
-//                  fifo_burst &history_fifo,
-//                  real_t history[MAX_SMP_PER_IMG*(NBOXCAR-1)]
-//                  ){  
-//  int i;
-//  int j;
-//  int loc;
-//  burst_real burst;
-//  const int max_burst_history_per_dm = MAX_BURST_HISTORY_PER_DM;
-//#pragma HLS DEPENDENCE variable=history inter false
-//#pragma HLS DEPENDENCE variable=history intra false
-//  
-// loop_fifo2history:
-//  for(i = 0; i < (NBOXCAR-1)*nburst_per_img; i++){
-//#pragma HLS PIPELINE
-//#pragma HLS LOOP_TRIPCOUNT max = max_burst_history_per_dm
-//    burst = history_fifo.read();
-//    for(j = 0; j < NREAL_PER_BURST; j++){
-//      loc = i*NREAL_PER_BURST+j;
-//      history[loc] = burst.data[j];
-//    }
-//  }
-//}
-
 void fifo2history(
-                  int nburst_per_img,
                   fifo_burst &history_fifo,
-                  real_t history[MAX_SMP_PER_IMG*(NBOXCAR-1)]
+                  real_t history[NSMP_PER_IMG*(NBOXCAR-1)]
                   ){  
   int i;
   int j;
   int loc;
   burst_real burst;
   const int max_burst_history_per_dm = MAX_BURST_HISTORY_PER_DM;
-#pragma HLS DEPENDENCE variable=history inter false
-#pragma HLS DEPENDENCE variable=history intra false
   
  loop_fifo2history:
-  for(i = 0; i < (NBOXCAR-1)*nburst_per_img; i++){
+  for(i = 0; i < (NBOXCAR-1)*NBURST_PER_IMG; i++){
 #pragma HLS PIPELINE
 #pragma HLS LOOP_TRIPCOUNT max = max_burst_history_per_dm
     burst = history_fifo.read();
@@ -198,8 +171,7 @@ void fifo2history(
 }
 
 void history2fifo(
-                  int nburst_per_img,
-                  real_t history[MAX_SMP_PER_IMG*(NBOXCAR-1)],
+                  real_t history[NSMP_PER_IMG*(NBOXCAR-1)],
                   fifo_burst &history_fifo
                   ){  
   int i;
@@ -207,11 +179,9 @@ void history2fifo(
   int loc;
   burst_real burst;
   const int max_burst_history_per_dm = MAX_BURST_HISTORY_PER_DM;
-#pragma HLS DEPENDENCE variable=history inter false
-#pragma HLS DEPENDENCE variable=history intra false
   
  loop_history2fifo:
-  for(i = 0; i < (NBOXCAR-1)*nburst_per_img; i++){
+  for(i = 0; i < (NBOXCAR-1)*NBURST_PER_IMG; i++){
 #pragma HLS PIPELINE
 #pragma HLS LOOP_TRIPCOUNT max = max_burst_history_per_dm
     for(j = 0; j < NREAL_PER_BURST; j++){
@@ -220,4 +190,12 @@ void history2fifo(
     }
     history_fifo.write(burst);
   }
+}
+
+void process_dm(
+                int ntime,
+                stream_burst &in, 
+                real_t history[NSMP_PER_IMG*(NBOXCAR-1)],
+                real_t threshold,
+                burst_real *out){
 }
